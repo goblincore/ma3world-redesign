5dd0ff983677e2feaef5c430f53112e7518df748
Donny <donny@Donnys-MacBook-Air.local>
2025-12-30 18:05:06 -0500

    Configure Github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // SITE: Replace with your actual domain or GitHub Pages URL
+  site: 'https://goblincore.github.io',
+  // BASE: The name of your repository /ma3world-redesign/
+  base: '/ma3world-redesign/',
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
