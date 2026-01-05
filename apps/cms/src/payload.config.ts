71ecd3cd48edbb4dedbcce4f0fd53a30da6ba481
Donny <donny@Donnys-MacBook-Air.local>
2026-01-05 16:50:55 -0500

    Update s3 config and env example

--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -40,12 +40,13 @@ export default buildConfig({
         media: true,
       },
       bucket: process.env.S3_BUCKET || '',
+      acl: null as any,
       config: {
         credentials: {
           accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
         },
-        region: process.env.S3_REGION || 'ap-southeast-1',
+        region: process.env.S3_REGION || 'ap-south-1',
         endpoint: process.env.S3_ENDPOINT || '',
         forcePathStyle: true,
       },
